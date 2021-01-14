1b44c4bea53e3bdab6d49072d424b3fc9cd08653
wmdorado <a2017210277@estudiantes.upsa.edu.bo>
2021-01-14 01:39:44 +0100

    Get_suma

---
+++ SoluciónSuma/WebSuma/Controllers/SumaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSuma.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SumaController : Controller
+    {
+         [HttpGet]
+      public int suma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
